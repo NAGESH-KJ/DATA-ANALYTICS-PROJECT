1acc8a27e6aa06538d6f031c1a6f317e52868807
AmruthKarnam <amruthkarnam@gmail.com>
2019-11-23 12:33:57 +0530

    Final

--- hyp_testing.py
+++ hyp_testing.py
@@ -1,38 +1,34 @@
-
-# coding: utf-8
-
-# In[8]:
-
-
-import pandas as pd
+# A z test is applied since a moderate sized  sample of the dataset is taken at random
 import numpy as np
-from scipy.stats import ttest_ind
+import pandas as pd
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+import scipy.stats as stats 
+from scipy.special import ndtr as ndtr
 #Hypothesis testing 
 # H0: Number of crimes in day > = crimes in night
 # H1: Number of crimes in day < number of crimes in night.
 df=pd.read_csv("incidents-100k.csv")
-# taking a random sample of the data
-randomSample = df.sample(n=60)
-l=df.is_night.unique()
-list1=[]
-# count the number of crimes taking plce during day and night respectively 
-for i in l:
-    subDf= randomSample.loc[df.is_night==i,]
-    list1.append(subDf.shape[0])
-day=list1[0]
-night=list1[1]
-print(day)
-print(night)
-#finding the p value
-ttest,pval = ttest_ind(day,night)
+#A list for number of crimes in days
+mean=df.is_night.mean()
+#This is to know The mean  and standared deviation of the sampling distribution of sample means.
+sigma=df.is_night.std(ddof=0)
+# Take a random sample.
+randomSample = df.sample(n=50)
+x_bar=randomSample.is_night.mean()
+#Intial step is we assume NUll hypothsis is true , so  number of crimes in day = number of crimes in night.
+# Now we take the area in disagreement with the null hypothesis of the newly generated sample.
+z_critical = 1.96 #Standard confidence level
+N = 30
+SE = sigma/np.sqrt(N)
+z_stat = (x_bar - mean)/SE
+pval = 1- ndtr(z_stat)
+# applying the standard formulaes
 # if the pvalues is less than 0.05 then we reject the null hypothesis
-if pval <0.05:
+if pval < 0.05:
   print("we reject null hypothesis")
 else:
   print("we fail to reject null hypothesis")
 #So we see that we fail to reject that more crimes take place in day than in night
-
-#We expect that more crimes take place during the night than in the day.
-#When a hypothesis testing was carried out to  check if  more crimes occur during the night time.
-#We see observe that the p value obtained is much greater than 0.05 and we fail to reject the null hypothesis , stating that  we can’t assume that more crimes  occur during the night time.
-#This is because a lot of crimes took place at 3 am and 4 am or very early in the  morning when people were still sleeping.
+#More number crimes were occuring in 3 and 4 am ,due this reason we are getting the conclusion that we cant
+#infer that crimes in night is greater than crimes in day time.
